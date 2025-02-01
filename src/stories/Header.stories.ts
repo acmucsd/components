9d5f2255d69a8769aad68b6e031450d5df143a25
= <aniket.9upt@gmail.com>
2025-02-01 12:29:52 -0800

    reorganized project and added index.ts

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -1,6 +1,6 @@
 import { fn } from '@storybook/test';
 
-import { Header } from '../components/Header';
+import { Header } from '../components/header/Header';
 
 export default {
   title: 'Example/Header',
