458c312753daa6392114fe82c472d33189071df2
= <aniket.9upt@gmail.com>
2025-05-05 14:43:13 -0700

    added radio and checkbox

---
+++ src/stories/Radio.stories.ts
@@ -0,0 +1,123 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Radio } from '../components/radio/Radio';
+
+const meta: Meta<typeof Radio> = {
+  title: 'Example/Radio',
+  component: Radio,
+  tags: ['autodocs'],
+  // Show changes in the "Actions" panel
+  argTypes: {
+    onChange: { action: 'changed' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Radio>;
+
+// Below are a few example stories. You can add more to show 
+// hover, focus, or pressed states by using pseudo-states 
+// or custom decorators.
+
+export const EnabledUnselectedLight: Story = {
+  args: {
+    checked: false,
+    disabled: false,
+    mode: 'light',
+    label: 'Label',
+  },
+};
+
+export const EnabledSelectedLight: Story = {
+  args: {
+    checked: true,
+    disabled: false,
+    mode: 'light',
+    label: 'Label',
+  },
+};
+
+export const DisabledUnselectedLight: Story = {
+  args: {
+    checked: false,
+    disabled: true,
+    mode: 'light',
+    label: 'Label',
+  },
+};
+
+export const DisabledSelectedLight: Story = {
+  args: {
+    checked: true,
+    disabled: true,
+    mode: 'light',
+    label: 'Label',
+  },
+};
+
+export const EnabledUnselectedDark: Story = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#25262B' }
+      ]
+    },
+  },
+  args: {
+    checked: false,
+    disabled: false,
+    mode: 'dark',
+    label: 'Label',
+  },
+};
+
+export const EnabledSelectedDark: Story = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#25262B' }
+      ]
+    },
+  },
+  args: {
+    checked: true,
+    disabled: false,
+    mode: 'dark',
+    label: 'Label',
+  },
+};
+
+export const DisabledUnselectedDark: Story = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#25262B' }
+      ]
+    },
+  },
+  args: {
+    checked: false,
+    disabled: true,
+    mode: 'dark',
+    label: 'Label',
+  },
+};
+
+export const DisabledSelectedDark: Story = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#25262B' }
+      ]
+    },
+  },
+  args: {
+    checked: true,
+    disabled: true,
+    mode: 'dark',
+    label: 'Label',
+  },
+};
